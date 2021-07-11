0aa20a9fcd006d68108fac92797059fa30151590
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-07-12 07:53:12 +0900

    4-5

--- template-ph3-study/app/Http/Controllers/HelloController.php
+++ template-ph3-study/app/Http/Controllers/HelloController.php
@@ -4,41 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
+use App\Http\Middleware\HelloMiddleware;
 
 global $head, $style, $body, $end;
-$head = '<html></html>';
-$style = <<<EOF
-<style>
-body {
-    font-size: 16px;;
-    color:#999;
-}
-h1 {
-    text-align: right;
-    color: #eee;
-    margin: -40px 0px -50px 0px;
-    font-size: 100px;
-}
-</style>
-EOF;
+
 
 class HelloController extends Controller
 {
-    // public function index(Request $request){
-    //     $data = [
-    //         'msg'=>'これはコントローラから渡されたメッセージです。',
-    //         'id'=>$request->id
-    //     ];
-    //     return view('hello.index', $data);
-    // }
-    public function index()
+
+    public function index(Request $request)
     {
-        // $data = [
-        //     ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
-        //     ['name'=>'田中はなこ', 'mail'=>'hanako@yamada'],
-        //     ['name'=>'鈴木さちこ', 'mail'=>'sachico@yamada'],
-        // ];
-        return view('hello.index', ['message'=>'Hello!']);
+        return view('hello.index', ['data'=>$request->data]);
     }
 }
