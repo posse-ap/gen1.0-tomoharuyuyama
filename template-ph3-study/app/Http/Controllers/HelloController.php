d537470135fea9d9517804cd3ae239de1c494e2c
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-07-12 09:52:54 +0900

    4-23

--- template-ph3-study/app/Http/Controllers/HelloController.php
+++ template-ph3-study/app/Http/Controllers/HelloController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Middleware\HelloMiddleware;
+use App\Http\Requests\HelloRequest;
 
 global $head, $style, $body, $end;
 
 
 class HelloController extends Controller
 {
-
+    
     public function index(Request $request)
     {
-        return view('hello.index');
+        return view('hello.index', ['msg'=>'フォームを入力：']);
     }
+
+
+    public function post(HelloRequest $request)
+    {
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
+    }
+
 }
--- template-ph3-study/app/Http/Kernel.php
+++ template-ph3-study/app/Http/Kernel.php
@@ -41,6 +41,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'helo' => [
+            \App\Http\Middleware\HelloMiddleware::class,
+        ],
     ];
 
     /**
