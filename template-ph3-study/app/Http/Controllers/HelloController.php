92f3491a8ccb6a48ac07c602406e84f41e7690b5
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-07-12 11:56:49 +0900

    4-32

--- template-ph3-study/app/Http/Controllers/HelloController.php
+++ template-ph3-study/app/Http/Controllers/HelloController.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Middleware\HelloMiddleware;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 global $head, $style, $body, $end;
 
 
 class HelloController extends Controller
 {
-    
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力：']);
+        return view('hello.index', ['msg'=>'フォームを入力下さい。']);
     }
-
 
     public function post(HelloRequest $request)
     {
--- template-ph3-study/app/Http/Requests/HelloRequest.php
+++ template-ph3-study/app/Http/Requests/HelloRequest.php
@@ -9,7 +9,7 @@ class HelloRequest extends FormRequest
     public function authorize()
     {
         if ($this->path() ==  'hello')
-        {
+            {
             return true;
         } else {
             return false;
@@ -21,18 +21,20 @@ class HelloRequest extends FormRequest
         return [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
+            'age' => 'numeric|hello',
         ];
     }
 
     public function messages()
-{
-   return [
-       'name.required' => '名前は必ず入力して下さい。',
-       'mail.email'  => 'メールアドレスが必要です。',
-       'age.numeric' => '年齢を整数で記入下さい。',
-       'age.between' => '年齢は０～150の間で入力下さい。',
-   ];
+    {
+        return [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email'  => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入下さい。',
+            'age.hello' => 'Hello! 入力は偶数のみ受け付けます。',
+        ];
+    }
+
 }
-}
+
 
