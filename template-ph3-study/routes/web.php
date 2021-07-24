ad11dd4ace5d709d217b9820ac67723ce2106664
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-07-24 15:59:24 +0900

    5-16

--- template-ph3-study/routes/web.php
+++ template-ph3-study/routes/web.php
@@ -18,6 +18,13 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
 Route::get('quiz', 'quizyController@index');
 Route::get('quiz/1', 'quizyController@quizy1');
 Route::get('quiz/2', 'quizyController@quizy2');
