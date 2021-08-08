ccd196a2c8ff855e764b666cf2d3b0052bfab8f4
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-08-08 17:37:29 +0900

    7-19

--- template-ph3-study/routes/web.php
+++ template-ph3-study/routes/web.php
@@ -25,6 +25,9 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
+Route::get('hello/rest', 'HelloController@rest');
+Route::get('hello/session', 'HelloController@ses_get');
+Route::post('hello/session', 'HelloController@ses_put');
 Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
@@ -37,6 +40,7 @@ Route::post('person/del', 'PersonController@remove');
 Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+Route::resource('rest', 'RestappController');
 
 
 Route::get('quiz', 'quizyController@index');
