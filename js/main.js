be53916d03c0003510111427d36860a67607174f
tomoharuyuyama <tomoharuyuyama@gmail.com>
2021-12-05 01:21:32 +0900

    一応完成。 保存ボタンを押したらinputを閉じたいが閉じれない。

---
+++ js/main.js
@@ -0,0 +1,68 @@
+(function () {
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      editedItem: '',
+      todos: []
+    },
+    watch: {
+      todos: {
+        handler: function () {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+    mounted: function () {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+    methods: {
+      addItem: function () {
+        var item = {
+          title: this.newItem,
+          isDone: false,
+          isActive: true,
+          tmp: ''
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function (index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      },
+      active: function (index) {
+        console.log('通電');
+        console.log(this.todos[index].isActive);
+        this.todos[index].isActive = !this.todos[index].isActive;
+        // }
+      },
+      editItem: function (index) {
+        console.log(this.editItem);
+        // if (confirm('are you sure?')) {
+        // //   // TODOがゴリラになるプログラム↓
+        // console.log(this.todos[index].title);
+        this.todos[index].title = this.todos[index].tmp;
+        this.todos[index].tmp = "";
+        // }
+      },
+      purge: function () {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        this.todos = this.remaining;
+      }
+    },
+    computed: {
+      remaining: function () {
+        return this.todos.filter(function (todo) {
+          return !todo.isDone;
+        });
+      }
+    }
+  });
+})();
