6a94ca05efcc8c4170e4db8d5e708de4b49be821
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-03-16 10:49:07 +0900

    ユーザーのシーダー設定

---
+++ src/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => '1',
+                'name' => 'ゆやま',
+                'email' => 'test@com',
+                'password' => 'password',
+            ],
+        ]);
+    }
+}
